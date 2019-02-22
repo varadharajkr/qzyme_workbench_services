0193423178c4a43fd318351a2e74d38a1730de38
varda <varadharajk.r@gmail.com>
2019-02-22 16:38:54 +0530

    MD RUN update version 2

--- Commands/views.py
+++ Commands/views.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+import datetime
+import sys
+
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
@@ -352,6 +356,65 @@ def sol_group_option():
     return SOL_option_value[0]
 
 
+@csrf_exempt
+def md_simulation_preparation(request, project_id, command_tool, command_title):
+    print "inside md_simulation_preparation function"
+    user_id = request.user.id
+    entry_time = datetime.now()
+    status = config.PATH_CONFIG['status']
+    QzwProjectDetails_res = \
+        QzwProjectDetails.objects.get(project_id=project_id)
+    project_name = QzwProjectDetails_res.project_name
+    project_name = str(project_name)
+    key_name = 'md_no_of_runs'
+
+    ProjectToolEssentials_res = \
+        ProjectToolEssentials.objects.all().filter(project_id=project_id,
+                                                   key_name=key_name).latest('entry_time')
+
+    md_run_no_of_conformation = ProjectToolEssentials_res.values
+
+    source_file_path = config.PATH_CONFIG['shared_folder_path'] + 'Project' + project_name + '/' + command_tool + '/'
+    for i in range(5):
+        print (source_file_path + '/md_run' + str(i + 1))
+        os.mkdir(source_file_path + '/md_run' + str(i + 1))
+        dest_file_path = source_file_path + '/md_run' + str(i + 1)
+        for file_name in os.listdir(source_file_path):
+            try:
+                print "inside try"
+                shutil.copy(str(source_file_path) + file_name, dest_file_path)
+            except IOError as e:
+                print("Unable to copy file. %s" % e)
+                pass
+            except Exception:
+                print("Unexpected error:", sys.exc_info())
+                pass
+        os.chdir(source_file_path + '/md_run' + str(i + 1))
+        os.system("gmx editconf -f complex_out.gro -o  newbox.gro -bt cubic -d 1.2")
+        os.system("gmx solvate -cp newbox.gro -cs spc216.gro -p topol.top -o solve.gro")
+        os.system("echo q | gmx make_ndx -f solve.gro > gromacs_solve_gro_indexing.txt")
+        os.system("gmx grompp -f ions.mdp -po mdout.mdp -c solve.gro -p topol.top -o ions.tpr")
+        group_value = sol_group_option()
+        SOL_replace_backup = "echo %SOL_value% | gmx genion -s ions.tpr -o solve_ions.gro -p topol.top -neutral"
+        SOL_replace_str = SOL_replace_backup
+        SOL_replace_str = SOL_replace_str.replace('%SOL_value%', group_value)
+        print("printing group value in MD$$$$$$$$$$$$$$$$$$")
+        print(group_value)
+        print("printing after %SOL% replace")
+        print(SOL_replace_str)
+        os.system(SOL_replace_str)
+        os.system("echo q | gmx make_ndx -f solve_ions.gro")
+        os.system("gmx grompp -f em.mdp -po mdout.mdp -c solve_ions.gro -p topol.top -o em.tpr")
+        os.system("gmx mdrun -v -s em.tpr -o em.trr -cpo em.cpt -c em.gro -e em.edr -g em.log -deffnm em")
+        os.system("gmx grompp -f nvt.mdp -po mdout.mdp -c em.gro -r em.gro -p topol.top -o nvt.tpr -n index.ndx")
+        os.system("gmx mdrun -v -s nvt.tpr -o nvt.trr -cpo nvt.cpt -c nvt.gro -e nvt.edr -g nvt.log -deffnm nvt")
+        os.system("gmx grompp -f npt.mdp -po mdout.mdp -c nvt.gro -r nvt.gro -p topol.top -o npt.tpr -n index.ndx")
+        os.system("gmx mdrun -v -s npt.tpr -o npt.trr -cpo npt.cpt -c npt.gro -e npt.edr -g npt.log -deffnm npt")
+        os.system("gmx grompp -f md.mdp -po mdout.mdp -c npt.gro -p topol.top -o md_0_1.tpr -n index.ndx")
+        os.system(
+            "gmx mdrun -v -s md_0_1.tpr -o md_0_1.trr -cpo md_0_1.cpt -x md_0_1.xtc -c md_0_1.gro -e md_0_1.edr -g md_0_1.log -deffnm md_0_1")
+
+
 #Substrate Parameterization
 class Complex_Simulations(APIView):
     string_data = " SOL "
@@ -379,6 +442,7 @@ class Complex_Simulations(APIView):
         primary_command_runnable =re.sub("%input_folder_name%",config.PATH_CONFIG['local_shared_folder_path']+project_name+'/'+commandDetails_result.command_tool+'/',primary_command_runnable)
         primary_command_runnable = re.sub('%output_folder_name%', config.PATH_CONFIG['local_shared_folder_path']+ project_name + '/' + commandDetails_result.command_tool + '/',primary_command_runnable)
         primary_command_runnable = re.sub('%input_output_folder_name%', config.PATH_CONFIG['local_shared_folder_path'] + project_name + '/' + commandDetails_result.command_tool +'/', primary_command_runnable)
+        primary_command_runnable = re.sub('python run_md.py', '', primary_command_runnable)
         os.chdir(config.PATH_CONFIG['local_shared_folder_path'] + project_name + '/' + commandDetails_result.command_tool +'/')
         print os.system("pwd")
         print os.getcwd()
@@ -393,6 +457,7 @@ class Complex_Simulations(APIView):
             primary_command_runnable = re.sub('%SOL_value%',group_value,
                                               primary_command_runnable)
         if commandDetails_result.command_title == "md_run":
+            md_simulation_preparation(request)
             # print config.PATH_CONFIG['local_shared_folder_path'] + project_name + '/' + commandDetails_result.command_tool +'/'
             # dir_value = config.PATH_CONFIG['local_shared_folder_path'] + project_name + '/' + commandDetails_result.command_tool +'/'
             # os.system("rm "+dir_value+"/index.ndx")
