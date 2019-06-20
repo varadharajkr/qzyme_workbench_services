fcd530c134152b1a92e468a02f28ae8ee6acc15d
sagar kalmesh <sagar.k.6789@gmail.com>
2019-06-20 14:38:35 +0530

    logger settings updated

--- gromacsCommands/settings.py
+++ gromacsCommands/settings.py
@@ -142,7 +142,7 @@ LOGGING = {
     'handlers': {
         'null': {
             'level':'DEBUG',
-            'class':'django.utils.log.NullHandler',
+            'class':'logging.NullHandler',
         },
         'logfile': {
             'level':'DEBUG',
