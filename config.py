517d6a153cb15286540b85603d247b3918d375ac
sagar kalmesh <sagar.k.6789@gmail.com>
2019-12-20 17:05:11 +0530

    config file update

---
+++ config.py
@@ -0,0 +1,37 @@
+PATH_CONFIG ={
+'python_sh_path':"/usr/local/MGLTools-1.5.6/bin/",
+'ligand_parametrization_path': "Ligand_Parametrization",
+'prepare_ligand4_py_path':"/usr/local/MGLTools-1.5.6/MGLToolsPckgs/AutoDockTools/Utilities24/",
+'shared_folder_path':"/mnt/Windows-Share/Project/",
+'local_shared_folder_path':"/mnt/Windows-Share/Project/",
+'add_python_file_path':"/mnt/Windows-Share/Project/Takakura/Autodock/",
+'local_shared_folder_path_project':"/mnt/Windows-Share/",
+'make_gpf_dpf_python_file_path':"/mnt/Windows-Share/Project/Takakura/Autodock/",
+'multiple_torsion_python_file_path':"/mnt/Windows-Share/Project/Takakura/Autodock/",
+'multiple_angle_python_file_path':"/mnt/Windows-Share/Project/Takakura/Autodock/",
+'multiple_distance_python_file_path':"/mnt/Windows-Share/Project/Takakura/Autodock/",
+ 'distance_python_file':"Distance/inputFiles/",
+ 'angle_python_file':"Angle/inputFiles/",
+'torsion_python_file':"Torsion/inputFiles/",
+'local_autodock_folder_path':"qzw/autodock/" ,
+'local_gromacs_folder_path':"qzw/gromacs/",
+'shared_folder_project_path':"/mnt/Windows-Share/Project/",
+'local_qzw_folder_path':"qzw/",
+'receptor_file_path':"/home/ck/Downloads/mgltools_x86_64Linux2_1.5.6/MGLToolsPckgs/AutoDockTools/Utilities24/",
+'ligand_prepare_file_path':"/home/qzyme/Downloads/mgltools_x86_64Linux2_1.5.6/MGLToolsPckgs/AutoDockTools/Utilities24/",
+'input_output_folder_name':"/mnt/Windows-Share/Project/",
+'grid_dock_map_python_file_path':"/mnt/Windows-Share/Project/Takakura/Autodock/",
+'mmtsb_path':"/scratch/toolset/perl/",
+'shared_scripts':"/mnt/Windows-Share/qz-workbench-scripts/",
+'mmpbsa_project_path':'Analysis/MMPBSA/',
+'md_simulations_path':'CatMec/MD_Simulation/',
+'GMX_run_file_one': '/home/research/qzpl1805/GMXPBSAtool/gmxpbsa0.sh',
+'GMX_run_file_two': '/home/research/qzpl1805/GMXPBSAtool/gmxpbsa1.sh',
+'GMX_run_file_three': '/home/research/qzpl1805/GMXPBSAtool/gmxpbsa2.sh'
+}
+
+CONSTS ={
+    'status_success':3,
+    'status_error':4,
+    'status_initiated':2
+}
