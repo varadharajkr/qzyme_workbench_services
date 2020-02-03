61de4425150f68841448f7b62c2e67676512ec77
varadharaj <varadharajk.r@gmail.com>
2020-02-03 11:22:07 +0530

    module load and unload of mmtsb for ligand parameterization module

--- Commands/views.py
+++ Commands/views.py
@@ -6795,6 +6795,8 @@ class CatMec(APIView):
                     update_command_status(inp_command_id, status_id)
                 return JsonResponse({"success": False, 'output': err, 'process_returncode': process_return.returncode})
         elif command_tool_title == "Ligand_Parametrization":
+            exec(open('/usr/share/Modules/init/python.py').read())
+            module('unload mmtsb')
             print(command_tool_title)
             inp_command_id = request.POST.get("command_id")
             commandDetails_result = commandDetails.objects.get(command_id=inp_command_id)
@@ -6861,6 +6863,7 @@ class CatMec(APIView):
                     db.close_old_connections()
                     status_id = config.CONSTS['status_success']
                     update_command_status(inp_command_id, status_id)
+                module('load mmtsb')
                 return JsonResponse({"success": True, 'output': out, 'process_returncode': process_return.returncode})
             if process_return.returncode != 0:
                 print("inside error")
@@ -6878,6 +6881,7 @@ class CatMec(APIView):
                     db.close_old_connections()
                     status_id = config.CONSTS['status_error']
                     update_command_status(inp_command_id, status_id)
+                module('load mmtsb')
                 return JsonResponse({"success": False, 'output': err, 'process_returncode': process_return.returncode})
         elif command_tool_title == "get_make_complex_parameter_details" or command_tool_title == "make_complex_params" or command_tool_title == "md_run":
             print('command_tool_title ----------------------\n')
