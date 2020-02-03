3715b7e2e91e29918add7ab2fe1abed8e627fb3e
varadharaj <varadharajk.r@gmail.com>
2020-02-03 19:10:26 +0530

    mgl tools and unload update version 11

--- Commands/views.py
+++ Commands/views.py
@@ -47,8 +47,6 @@ import logging # for default django logging
 
 django_logger = logging.getLogger(__name__)
 
-# module accessing
-exec(open('/usr/share/Modules/init/python.py').read())
 
 # to run command in shell
 def execute_command(command,inp_command_id):
@@ -6799,12 +6797,14 @@ class CatMec(APIView):
                     update_command_status(inp_command_id, status_id)
                 return JsonResponse({"success": False, 'output': err, 'process_returncode': process_return.returncode})
         elif command_tool_title == "Ligand_Parametrization":
-            print('exec(open(/usr/share/Modules/init/python.py).read())')
+            '''print('exec(open(/usr/share/Modules/init/python.py).read())')
             try:
+                # module accessing
+                exec(open('/usr/share/Modules/init/python.py').read())
                 print('module(unload mgltools)')
                 module('unload mgltools')
             except Exception as e:
-                print("inside exception of unload mgltools in LP ",str(e))
+                print("inside exception of unload mgltools in LP ",str(e))'''
             print('command_tool_title is ',command_tool_title)
             inp_command_id = request.POST.get("command_id")
             commandDetails_result = commandDetails.objects.get(command_id=inp_command_id)
@@ -6871,12 +6871,14 @@ class CatMec(APIView):
                     db.close_old_connections()
                     status_id = config.CONSTS['status_success']
                     update_command_status(inp_command_id, status_id)
-
+                '''
                 try:
+                    # module accessing
+                    exec (open('/usr/share/Modules/init/python.py').read())
                     print('module(load mgltools)')
                     module('load mgltools')
                 except Exception as e:
-                    print("inside exception of load mgltools in LP ", str(e))
+                    print("inside exception of load mgltools in LP ", str(e))'''
                 return JsonResponse({"success": True, 'output': out, 'process_returncode': process_return.returncode})
             if process_return.returncode != 0:
                 print("inside error")
@@ -6894,11 +6896,14 @@ class CatMec(APIView):
                     db.close_old_connections()
                     status_id = config.CONSTS['status_error']
                     update_command_status(inp_command_id, status_id)
+                '''
                 try:
+                    # module accessing
+                    exec (open('/usr/share/Modules/init/python.py').read())
                     print('module(load mgltools)')
                     module('load mgltools')
                 except Exception as e:
-                    print("inside exception of load mgltools in LP ", str(e))
+                    print("inside exception of load mgltools in LP ", str(e))'''
                 return JsonResponse({"success": False, 'output': err, 'process_returncode': process_return.returncode})
         elif command_tool_title == "get_make_complex_parameter_details" or command_tool_title == "make_complex_params" or command_tool_title == "md_run":
             print('command_tool_title ----------------------\n')
