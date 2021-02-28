32a44048f19473859091622293c91037894f5cf5
varadharaj <varadharajk.r@gmail.com>
2021-03-01 01:52:56 +0530

    Thermostability implementation

--- Commands/views.py
+++ Commands/views.py
@@ -824,59 +824,60 @@ def queue_slurm_script_of_thermostability(user_id,project_id,file_path,pre_std_f
     print("Converting from windows to unix format")
     print("perl -p -e 's/\r$//' < "+str(file_path)+str(pre_std_file_name)+" > "+str(file_path)+str(file_name))
     os.system("perl -p -e 's/\r$//' < "+str(file_path)+str(pre_std_file_name)+" > "+str(file_path)+str(file_name))
-    print('queuing **********************************************************************************')
-    print("sbatch "+ file_path + "/" + str(file_name))
-    #cmd = "srun "+ file_path + "/" + str(file_name)
-    cmd = "sbatch "+ file_path + "/" + str(file_name)
-    print("Submitting Job1 with command: %s" % cmd)
-    status, jobnum = commands.getstatusoutput(cmd)
-    print("job id is ", jobnum)
-    print("status is ", status)
-    print("job id is ", jobnum)
-    print("status is ", status)
-    print(jobnum.split())
-    lenght_of_split = len(jobnum.split())
-    index_value = lenght_of_split - 1
-    print(jobnum.split()[index_value])
-    job_id = jobnum.split()[index_value]
-    # save job id
-    job_id_key_name = "job_id"
-    entry_time = datetime.now()
-    try:
-        print(
-            "<<<<<<<<<<<<<<<<<<<<<<< in try of TASS PLOT ENERGY JOB SCHEDULING >>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        QzwSlurmJobDetails_save_job_id = QzwSlurmJobDetails(user_id=user_id,
-                                                            project_id=project_id,
-                                                            entry_time=entry_time,
-                                                            job_id=job_id,
-                                                            job_status="1",
-                                                            job_title='qzw_create_mutation',
-                                                            job_details='creating mutation')
-        QzwSlurmJobDetails_save_job_id.save()
-    except db.OperationalError as e:
-        print("<<<<<<<<<<<<<<<<<<<<<<< in except of TASS PLOT ENERGY JOB SCHEDULING >>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        db.close_old_connections()
-        QzwSlurmJobDetails_save_job_id = QzwSlurmJobDetails(user_id=user_id,
-                                                            project_id=project_id,
-                                                            entry_time=entry_time,
-                                                            job_id=job_id,
-                                                            job_status="1",
-                                                            job_title='qzw_create_mutation',
-                                                            job_details='creating mutation')
-        QzwSlurmJobDetails_save_job_id.save()
-        print("saved")
-    except Exception as e:
-        print("<<<<<<<<<<<<<<<<<<<<<<< in except of TASS PLOT ENERGY JOB SCHEDULING >>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        print("exception is ",str(e))
-        pass
-        '''QzwSlurmJobDetails_save_job_id = QzwSlurmJobDetails(user_id=user_id,
-                                                                               project_id=project_id,
-                                                                               entry_time=entry_time,
-                                                                               values=job_id,
-                                                                               job_id=job_id)
-        QzwSlurmJobDetails_save_job_id.save()
-        print("saved")'''
-    print('queued')
+    
+    # print('queuing **********************************************************************************')
+    # print("sbatch "+ file_path + "/" + str(file_name))
+    # #cmd = "srun "+ file_path + "/" + str(file_name)
+    # cmd = "sbatch "+ file_path + "/" + str(file_name)
+    # print("Submitting Job1 with command: %s" % cmd)
+    # status, jobnum = commands.getstatusoutput(cmd)
+    # print("job id is ", jobnum)
+    # print("status is ", status)
+    # print("job id is ", jobnum)
+    # print("status is ", status)
+    # print(jobnum.split())
+    # lenght_of_split = len(jobnum.split())
+    # index_value = lenght_of_split - 1
+    # print(jobnum.split()[index_value])
+    # job_id = jobnum.split()[index_value]
+    # # save job id
+    # job_id_key_name = "job_id"
+    # entry_time = datetime.now()
+    # try:
+    #     print(
+    #         "<<<<<<<<<<<<<<<<<<<<<<< in try of TASS PLOT ENERGY JOB SCHEDULING >>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    #     QzwSlurmJobDetails_save_job_id = QzwSlurmJobDetails(user_id=user_id,
+    #                                                         project_id=project_id,
+    #                                                         entry_time=entry_time,
+    #                                                         job_id=job_id,
+    #                                                         job_status="1",
+    #                                                         job_title='qzw_create_mutation',
+    #                                                         job_details='creating mutation')
+    #     QzwSlurmJobDetails_save_job_id.save()
+    # except db.OperationalError as e:
+    #     print("<<<<<<<<<<<<<<<<<<<<<<< in except of TASS PLOT ENERGY JOB SCHEDULING >>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    #     db.close_old_connections()
+    #     QzwSlurmJobDetails_save_job_id = QzwSlurmJobDetails(user_id=user_id,
+    #                                                         project_id=project_id,
+    #                                                         entry_time=entry_time,
+    #                                                         job_id=job_id,
+    #                                                         job_status="1",
+    #                                                         job_title='qzw_create_mutation',
+    #                                                         job_details='creating mutation')
+    #     QzwSlurmJobDetails_save_job_id.save()
+    #     print("saved")
+    # except Exception as e:
+    #     print("<<<<<<<<<<<<<<<<<<<<<<< in except of TASS PLOT ENERGY JOB SCHEDULING >>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    #     print("exception is ",str(e))
+    #     pass
+    #     '''QzwSlurmJobDetails_save_job_id = QzwSlurmJobDetails(user_id=user_id,
+    #                                                                            project_id=project_id,
+    #                                                                            entry_time=entry_time,
+    #                                                                            values=job_id,
+    #                                                                            job_id=job_id)
+    #     QzwSlurmJobDetails_save_job_id.save()
+    #     print("saved")'''
+    # print('queued')
 
     return True
 
