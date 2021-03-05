c729167ad9efdc4ea8a98fe301f597e923451ff2
varadharaj <varadharajk.r@gmail.com>
2021-03-05 19:47:08 +0530

    TASS script changes

--- Commands/views.py
+++ Commands/views.py
@@ -1231,6 +1231,8 @@ class Thermostability(APIView):
             shutil.copyfile(os.path.join(qz_workbench_script_path,"matrix_generation.py"),os.path.join(destination_file_path,"matrix_generation.py"))
             shutil.copyfile(os.path.join(qz_workbench_script_path,"create_mutate_std.sh"),os.path.join(destination_file_path,"create_mutate_std.sh"))
             shutil.copyfile(os.path.join(qz_workbench_script_path,"rotabase.txt"),os.path.join(destination_file_path,"rotabase.txt"))
+            shutil.copyfile(os.path.join(qz_workbench_script_path,"generate_mean_variant_plot.py"),os.path.join(destination_file_path,"generate_mean_variant_plot.py"))
+            shutil.copyfile(os.path.join(qz_workbench_script_path,"generate_energy_plot.py"),os.path.join(destination_file_path,"generate_energy_plot.py"))
         except Exception as e:
             print(('exception in copying file of mutation is ', str(e)))
             pass
